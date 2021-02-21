081251c532835d7ffc2616e8c65244a203b6600e
deeplify <navy.applee@gmail.com>
2021-02-21 19:48:18 +0900

    simulate batch

--- src/main/java/com/deeplify/tutorial/batch/jobs/TutorialConfig.java
+++ src/main/java/com/deeplify/tutorial/batch/jobs/TutorialConfig.java
@@ -2,15 +2,19 @@ package com.deeplify.tutorial.batch.jobs;
 
 import com.deeplify.tutorial.batch.tasklets.TutorialTasklet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class TutorialConfig {
@@ -21,15 +25,25 @@ public class TutorialConfig {
     @Bean
     public Job tutorialJob() {
         return jobBuilderFactory.get("tutorialJob")
-                .start(tutorialStep(null))
+                .start(tutorialStep1())
+                .next(tutorialStep2())
                 .build();
     }
 
     @Bean
-    @JobScope
-    public TaskletStep tutorialStep(@Value("#{jobParameters[datetime]}") String datetime) {
-        return stepBuilderFactory.get("tutorialStep")
-                .tasklet(new TutorialTasklet(datetime))
+    public Step tutorialStep1() {
+        return stepBuilderFactory.get("tutorialStep1")
+                .tasklet(new TutorialTasklet())
+                .build();
+    }
+
+    @Bean
+    public Step tutorialStep2() {
+        return stepBuilderFactory.get("tutorialStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.debug("I'm a tutorialStep2");
+                        return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 }
--- src/main/java/com/deeplify/tutorial/batch/tasklets/TutorialTasklet.java
+++ src/main/java/com/deeplify/tutorial/batch/tasklets/TutorialTasklet.java
@@ -1,19 +1,26 @@
 package com.deeplify.tutorial.batch.tasklets;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 
+@Slf4j
+@StepScope
 @RequiredArgsConstructor
 public class TutorialTasklet implements Tasklet {
 
-    private final String datetime;
+    @Value("#{jobParameters[datetime]}")
+    private String datetime;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        System.out.println("# executed tasklet !!");
+        log.debug("# executed tasklet !!");
+        log.debug("datetime: {}", datetime);
         return RepeatStatus.FINISHED;
     }
 }
